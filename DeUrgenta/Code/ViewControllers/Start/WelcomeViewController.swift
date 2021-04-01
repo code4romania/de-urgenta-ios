7853ff98d47ef68b0fcbcbdec16d8aa2709e2746
Cristi Habliuc <cristi.habliuc@gmail.com>
2021-04-01 18:46:15 +0300

    created project (#6)

---
+++ DeUrgenta/Code/ViewControllers/Start/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  DeUrgenta
+//
+//  Created by Cristi Habliuc on 01.04.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: DUViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
