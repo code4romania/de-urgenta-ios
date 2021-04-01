725eb82610bae828fc8f16cdff9c52c32b522dbc
Cristi Habliuc <cristi.habliuc@gmail.com>
2021-04-01 18:35:35 +0300

    created project

---
+++ DeUrgenta/Code/ViewControllers/Core/DUViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DUViewController.swift
+//  DeUrgenta
+//
+//  Created by Cristi Habliuc on 01.04.2021.
+//
+
+import UIKit
+
+/// Base class for screen view controllers
+class DUViewController: UIViewController{
+    
+}
