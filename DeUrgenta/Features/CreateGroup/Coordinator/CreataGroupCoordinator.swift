344ee431ab03167f3ebe86bbd81506d2a86058a5
Roberto <roberto.tiboldi@ulbisbiu.ro>
2022-06-20 11:32:32 +0300

    Add CreateGroupView

---
+++ DeUrgenta/Features/CreateGroup/Coordinator/CreataGroupCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  CreataGroupCoordinator.swift
+//  DeUrgenta
+//
+//  Created by Roberto on 16.06.2022.
+//
+
+import Foundation
---
+++ DeUrgenta/Features/CreateGroup/View/CreateGroupView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateGroupView.swift
+//  DeUrgenta
+//
+//  Created by Roberto on 16.06.2022.
+//
+
+import SwiftUI
+
+struct CreateGroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateGroupView()
+    }
+}
