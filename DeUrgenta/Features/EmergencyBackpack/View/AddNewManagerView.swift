ff7efb6342d42398a5a9d92e53e641dea88e0fd1
Tiboldi_Roberto <83965956+TiboldiRoberto@users.noreply.github.com>
2022-07-20 14:09:06 +0300

    Implement AddNewManagerView UI

--- DeUrgenta/Features/EmergencyBackpack/View/AddNewManagerView.swift
+++ DeUrgenta/Features/EmergencyBackpack/View/AddNewManagerView.swift
@@ -1,8 +1,71 @@
 import SwiftUI
 
 struct AddNewManagerView: View {
+    @State private var transferResourcesInNewBackpack = false
+
     var body: some View {
-        Text("Add Manager")
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text("Adaugă responsabil nou")
+                    .font(.custom("IBMPlexSans-SemiBold", size: 23))
+                    .frame(alignment: .leading)
+                    .padding(.vertical, 10)
+
+                VStack {
+                    Text("Această acțiune va crea un nou rucsac în profilul tau pentru a-l putea împărți cu alte persoane cu care locuiești. Poți invita noi responsabili fie dintre persoanele care sunt parte din grupul/grupurile tale sau o persoană complet nouă.")
+                        .modifier(PrimaryTextStyle())
+
+                    Text("Pare că ai adăugat deja resurse în rucsac. Bifează mai jos dacă vrei să transferi resursele din rucsacul tău în noul rucsac pe care îl împarți cu alte persoane.")
+                        .modifier(PrimaryTextStyle())
+                        .padding(.top, 15)
+                }
+                .lineSpacing(10)
+                .padding(.vertical, 10)
+
+                Toggle(isOn: $transferResourcesInNewBackpack, label: {
+                    Text("Transferă resurse în noul rucsac")
+                        .font(.custom("IBMPlexSans-Bold", size: 16))
+                        .foregroundColor(.black)
+                })
+                .padding(.vertical)
+
+                Text("Alege responsabil")
+                    .font(.custom("IBMPlexSans-SemiBold", size: 20))
+                    .foregroundColor(.darkText)
+                    .padding(.vertical)
+
+                VStack {
+                    Divider()
+                        .padding(.top, 10)
+
+                    Button(action: {
+                        // TODO: Implement this action
+                    }, label: {
+                        HStack {
+                            Image(systemName: "plus.circle.fill")
+                                .foregroundColor(.accent)
+
+                            Text(AppStrings.MyBackpackView.addManagerButton.localized())
+                                .font(.custom("IBMPlexSans-SemiBold", size: 20))
+                                .foregroundColor(.accent)
+                        }
+                        .padding(.vertical)
+
+                        Spacer()
+
+                        Image("chevron-right")
+                            .foregroundColor(.accent)
+                            .padding(.trailing, 10)
+                    })
+
+                    Divider()
+                }
+                .padding(.bottom, 20)
+
+                Spacer()
+            }
+            .padding(.horizontal)
+        }
     }
 }
 
