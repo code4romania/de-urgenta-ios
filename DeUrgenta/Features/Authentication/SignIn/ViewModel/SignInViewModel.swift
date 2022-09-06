cdd7a936ce70d053e5e04959216767637c3c3d71
Roberto <roberto.tiboldi@ulbisbiu.ro>
2022-09-06 13:50:11 +0300

    Add Validator

--- DeUrgenta/Features/Authentication/SignIn/ViewModel/SignInViewModel.swift
+++ DeUrgenta/Features/Authentication/SignIn/ViewModel/SignInViewModel.swift
@@ -5,4 +5,18 @@ class SignInViewModel: ObservableObject {
     @Published var password: String = ""
     @Published var showEmailError: Bool = false
     @Published var showPasswordError: Bool = false
+    
+    private let validator = Validator.shared
+
+    func isValidEmail() -> Bool {
+        let validationState = validator.isValidData(data: email, type: .email)
+        showEmailError = !validationState
+        return validationState
+    }
+
+    func isValidPassword() -> Bool {
+        let validationState = validator.isValidData(data: password, type: .password)
+        showPasswordError = !validationState
+        return validationState
+    }
 }
